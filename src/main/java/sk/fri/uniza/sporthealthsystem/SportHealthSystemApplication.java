3632a2355d5751c41f255b02efcd21caeb4394d3
Jozef Stasko <stasko.jozef427@gmail.com>
2021-10-23 14:26:17 +0200

    Add files via upload

---
+++ src/main/java/sk/fri/uniza/sporthealthsystem/SportHealthSystemApplication.java
@@ -0,0 +1,13 @@
+package sk.fri.uniza.sporthealthsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportHealthSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportHealthSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/fri/uniza/sporthealthsystem/SportHealthSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.fri.uniza.sporthealthsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportHealthSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
